806bf6206ac4c4ed2f31a5880cc34804bc39ae50
Paul Abraham <paul@blukaze.in>
2026-09-03 13:50:43 +0530

    style: align Blukaze brand SVG characters to uniform height and neat grid proportions

--- src/components/BlukazeBrand.tsx
+++ src/components/BlukazeBrand.tsx
@@ -12,7 +12,7 @@ export const BlukazeBrand: React.FC<BlukazeBrandProps> = ({
 }) => {
   return (
     <svg
-      viewBox="0 0 230 40"
+      viewBox="0 0 240 40"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
@@ -22,33 +22,35 @@ export const BlukazeBrand: React.FC<BlukazeBrandProps> = ({
     >
       <g
         stroke="currentColor"
-        strokeWidth="3.8"
+        strokeWidth="3.4"
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        {/* B - Futuristic geometric B with caps */}
-        <path d="M 6 6 H 22 C 28 6 28 19 22 19 H 6 M 6 19 H 24 C 30 19 30 34 24 34 H 6 V 6" />
-        <path d="M 2 6 H 6 M 2 34 H 6" strokeWidth="3" />
+        {/* B (X: 6 to 28) - Uniform height Y: 6 to 34 */}
+        <path d="M 6 6 V 34" />
+        <path d="M 6 6 H 20 C 26 6 26 20 20 20 H 6" />
+        <path d="M 6 20 H 21 C 27.5 20 27.5 34 21 34 H 6" />
 
-        {/* L / I - Clean vertical stem */}
-        <path d="M 44 6 V 34" />
+        {/* L (X: 40 to 58) - Uniform height Y: 6 to 34 */}
+        <path d="M 40 6 V 34 H 58" />
 
-        {/* U - Smooth rounded trough */}
-        <path d="M 62 6 V 23 C 62 31 78 31 78 23 V 6" />
+        {/* U (X: 70 to 92) - Uniform height Y: 6 to 34 */}
+        <path d="M 70 6 V 23 C 70 34 92 34 92 23 V 6" />
 
-        {/* K - Sharp angle bracket '<' */}
-        <path d="M 112 8 L 95 20 L 112 32" />
+        {/* K (X: 104 to 126) - Uniform height Y: 6 to 34 */}
+        <path d="M 104 6 V 34" />
+        <path d="M 126 6 L 105 20 L 126 34" />
 
-        {/* A - Upward chevron '^' (A without crossbar) */}
-        <path d="M 124 34 L 136 6 L 148 34" />
+        {/* A (X: 138 to 160) - Uniform height Y: 6 to 34 */}
+        <path d="M 138 34 L 149 6 L 160 34" />
 
-        {/* Z - Sharp geometric Z */}
-        <path d="M 160 6 H 178 L 160 34 H 178" />
+        {/* Z (X: 172 to 194) - Uniform height Y: 6 to 34 */}
+        <path d="M 172 6 H 194 L 172 34 H 194" />
 
-        {/* E - Three horizontal parallel bars '≡' */}
-        <path d="M 190 8 H 212" />
-        <path d="M 190 20 H 212" />
-        <path d="M 190 32 H 212" />
+        {/* E (X: 206 to 228) - Uniform height Y: 6 to 34 */}
+        <path d="M 206 6 H 228" />
+        <path d="M 206 20 H 228" />
+        <path d="M 206 34 H 228" />
       </g>
     </svg>
   );
